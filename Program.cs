6a576598902f926ef60deec922d52ccd0e82646f
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-09 21:25:15 -0500

    task done

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ObjectMethodsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Jim", "Business", 2.8);// created students and (objects)
+            Student student2 = new Student("Pam", "Art", 3.6);
+
+            Console.WriteLine(student1.hasHonors());
+            Console.WriteLine(student2.hasHonors());
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectMethodsPractice
+{
+    class Student
+
+    {
+        public string name;
+        public string major; // attributes
+        public double gpa;
+
+
+        public Student( string aNAme,string aMajor, double aGpa)// comstructor
+        {
+            name = aNAme;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+        public bool hasHonors()
+        {
+            if (gpa >= 2.6)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+
+    }
+}
